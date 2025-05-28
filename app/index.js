0e047ed239a6039b01899eb50ff06fd67ec20fff
Abhi Patel <abhippatel2024@gmail.com>
2025-05-28 17:08:58 -0400

    App Initiated

---
+++ app/index.js
@@ -0,0 +1,17 @@
+// app/index.js
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('✅ Hello from the CI/CD Pipeline app!');
+});
+
+app.get('/status', (req, res) => {
+  res.json({ status: 'ok', time: new Date() });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
+
