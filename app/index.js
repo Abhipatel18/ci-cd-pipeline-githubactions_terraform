69f97fad810e9687828388620bad43f65ef564b1
cleverstrone <35955092+cleverstrone@users.noreply.github.com>
2025-06-26 15:45:01 -0400

    Update index.js

--- app/index.js
+++ app/index.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
+const HOST = process.env.EC2_PUBLIC_IP || 'localhost';
 
 app.get('/', (req, res) => {
   res.send('Hello from the CI/CD Pipeline app!');
@@ -12,6 +13,7 @@ app.get('/status', (req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
+  // console.log(`Server running at http://localhost:${PORT}`);
+  console.log(`Server running at http://${HOST}:${PORT}`);
 });
 
